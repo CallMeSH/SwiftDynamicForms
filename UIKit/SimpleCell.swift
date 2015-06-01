4fd3e80aa5959d3ce4011df686afdde630f19f0e
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2015-06-01 11:27:19 +0200

    Dynamic tables

---
+++ UIKit/SimpleCell.swift
@@ -0,0 +1,35 @@
+//
+//  Cells.swift
+//  SCMBImages
+//
+//  Created by Benoit Pereira da Silva on 21/04/2015.
+//  Copyright (c) 2015 http://pereira-da-silva.com  All rights reserved.
+//
+
+import UIKit
+
+
+
+// A cell that does not require real configuration
+
+protocol SimpleCellDelegate{
+}
+
+class SimpleCellConfigurator:CellConfigurator{
+    var delegate:SimpleCellDelegate
+    init(delegate:SimpleCellDelegate){
+        self.delegate=delegate
+    }
+}
+
+class SimpleCell:UITableViewCell,Configurable{
+    
+    var configurator:SimpleCellConfigurator?
+    func configureWith(configurator:Configurator){
+        if let configuratorInstance = configurator as? SimpleCellConfigurator {
+            self.configurator = configuratorInstance
+        }else{
+            self.textLabel?.text="CellConfigurator required"
+        }
+    }
+}
